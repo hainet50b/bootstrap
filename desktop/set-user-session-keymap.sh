c7d9f4e2f9627ef3eead41fdd158d77b748bb3b8
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-06 18:04:39 +0900

    Fix keymap setup script to update mru-sources

--- desktop/set-user-session-keymap.sh
+++ desktop/set-user-session-keymap.sh
@@ -5,11 +5,14 @@ tmpdir=$(mktemp -d)
 trap 'rm -rf "$tmpdir"' EXIT
 
 gsettings get org.gnome.desktop.input-sources sources > "$tmpdir/before"
+gsettings get org.gnome.desktop.input-sources mru-sources >> "$tmpdir/before"
 
 layout=${1:-jp}
 gsettings set org.gnome.desktop.input-sources sources "[('xkb','$layout')]"
+gsettings set org.gnome.desktop.input-sources mru-sources "[('xkb','$layout')]"
 
 gsettings get org.gnome.desktop.input-sources sources > "$tmpdir/after"
+gsettings get org.gnome.desktop.input-sources mru-sources >> "$tmpdir/after"
 
 if diff -q "$tmpdir/before" "$tmpdir/after"; then
   echo 'No changes detected.'
