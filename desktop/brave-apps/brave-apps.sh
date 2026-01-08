c81d81597385eb12bb961510f57697a94a5fd3d3
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-08 22:09:20 +0900

    Fix output path to be relative to script location

--- desktop/brave-apps/brave-apps.sh
+++ desktop/brave-apps/brave-apps.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
 cat "$HOME/.local/share/applications"/brave-* \
   | sed -n 's/.*Name=\([^\n]*\).*/\1/p' \
-  > ./brave-apps.txt
+  | sort \
+  > "${script_dir}/brave-apps.txt"
 
