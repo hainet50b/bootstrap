363c7364695392130ef37b640106dad2567e88f8
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-27 19:01:56 +0900

    Add VSCodium install script

---
+++ desktop/vscodium.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -eu
+
+if command -v codium > /dev/null 2>&1; then
+  current_version=$(codium -v | head -n 1)
+else
+  current_version='0.0.0'
+fi
+releases=$(curl -fsSL \
+  -H 'Accept: application/vnd.github+json' \
+  https://api.github.com/repos/VSCodium/vscodium/releases/latest
+)
+latest_version=$(echo "$releases" | jq -r '.tag_name')
+
+install_and_update_vscodium() {
+  local tmpdir=$(mktemp -d)
+  trap 'rm -rf "$tmpdir"' RETURN
+
+  local assets=$(echo "$releases" | jq -r '.assets[]')
+
+  local bin_link=$(echo "$assets" \
+    | jq -r "select(.name == \"VSCodium-linux-x64-${latest_version}.tar.gz\") | .browser_download_url"
+  )
+  local checksum_link=$(echo "$assets" \
+    | jq -r "select(.name == \"VSCodium-linux-x64-${latest_version}.tar.gz.sha256\") | .browser_download_url"
+  )
+
+  cd "$tmpdir"
+
+  curl -fsSL -O "$bin_link"
+  curl -fsSL -O "$checksum_link"
+
+  sha256sum -c "VSCodium-linux-x64-${latest_version}.tar.gz.sha256"
+
+  local base_dir="${HOME}/.local"
+  local bin_dir="${base_dir}/bin"
+  local opt_dir="${base_dir}/opt/vscodium"
+
+  mkdir -p "$bin_dir"
+  mkdir -p "$opt_dir"
+
+  local current_dir="${opt_dir}/current"
+  local install_dir="${opt_dir}/${latest_version}"
+
+  mkdir -p "$install_dir"
+
+  tar -xzf "${tmpdir}/VSCodium-linux-x64-${latest_version}.tar.gz" -C "$install_dir"
+
+  ln -sfn "$install_dir" "$current_dir"
+  ln -sfn "${current_dir}/bin/codium" "${bin_dir}/codium"
+}
+
+if [ "$current_version" = '0.0.0' ]; then
+  echo "Installing VSCodium: ${latest_version}"
+  install_and_update_vscodium
+elif [ "$current_version" = "$latest_version" ]; then
+  echo "VSCodium is up to date: ${current_version}"
+else
+  echo "Updating VSCodium: ${current_version} -> ${latest_version}"
+  install_and_update_vscodium
+fi
+
